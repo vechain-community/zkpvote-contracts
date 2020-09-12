f27ca3b080562d0da45368cb4f9a738774cede28
Ziheng Zhou <zzmaczz@Zihengs-MacBook-Pro.local>
2020-09-12 17:19:34 +0800

    dl ballots, tally and submit

--- testnet/tally.ts
+++ testnet/tally.ts
@@ -2,16 +2,39 @@ import { Framework } from '@vechain/connex-framework'
 import { Driver, SimpleNet, SimpleWallet, options } from '@vechain/connex-driver'
 import { utils, connexutils } from 'myvetools'
 
-const fs = require('fs')
+import fs = require('fs')
+import BN from 'bn.js'
 
 import { pre } from './utils'
+import { toHex } from '../src/utils'
+import { CompressedBallot, uncompressBallot } from '../src/binary-ballot'
+import { Tally, prepTallyRes } from '../src/binary-tally'
+import { g } from '../src/ec'
 
 import {
     accounts,
     abiVotingContract,
-    infoFile,
-    tallyRes, invalidBallots
+    infoFile
 } from './init'
+
+
+function getCompressedBallot(out: Connex.Thor.VMOutput): CompressedBallot {
+    return {
+        h: toHex(out.decoded[0], 10),
+        y: toHex(out.decoded[1], 10),
+        zkp: [
+            toHex(out.decoded[2][0], 10),
+            toHex(out.decoded[2][1], 10),
+            toHex(out.decoded[2][2], 10),
+            toHex(out.decoded[2][3], 10),
+            toHex(out.decoded[2][4], 10),
+            toHex(out.decoded[2][5], 10),
+            toHex(out.decoded[2][6], 10),
+            toHex(out.decoded[2][7], 10),
+        ],
+        prefix: toHex(out.decoded[3], 10)
+    }
+}
 
 (async () => {
     const wallet = new SimpleWallet()
@@ -25,33 +48,78 @@ import {
     const addrVotingContract = info.addrVotingContract
     const voteID = info.voteID
 
+    let resp: Connex.Vendor.TxResponse, rec: Connex.Thor.Receipt
+
     // Start tally, VotingContract no longer accept new ballots
-    let resp = await connexutils.contractCallWithTx(
+    resp = await connexutils.contractCallWithTx(
         connex, auth, 100000,
         addrVotingContract, '0x0',
         utils.getABI(abiVotingContract, 'beginTally', 'function'),
         voteID
     )
-    let rec = await connexutils.getReceipt(connex, 5, resp.txid)
-    console.log('Tally starts:')
-    console.log('\ttxid: ', resp.txid)
-    console.log('\tgas used:', rec.gasUsed)
+    rec = await connexutils.getReceipt(connex, 5, resp.txid)
+    console.log(`Tally starts:
+    txid: ${resp.txid}
+    gas used: ${rec.gasUsed}`)
 
-    // The authority will off-chain download and validate all the ballots
-    // and tally and upload the result that can be verified by anyone.
-    const r = prepTallyRes(tallyRes)
+    /**
+     * The authority will off-chain download and validate all the ballots
+     * and tally and upload the result that can be verified by anyone.
+     */
+    
+    // Get the number of voter
+    let out = await connexutils.contractCall(
+        connex, addrVotingContract,
+        utils.getABI(abiVotingContract, 'getNumVoter', 'function'),
+        voteID
+    )
+    const n = parseInt(out.decoded[0])
+
+    // Prepare authority public and private keys
+    const k = new BN(info.k.slice(2), 'hex')
+    const gk = g.mul(k)
+
+    // New tally
+    const tally = new Tally(k, auth)
+
+    // Download, uncompress and count ballot
+    for (let i = 0; i < n; i++) {
+        // Get the address of the account that cast a ballot
+        out = await connexutils.contractCall(
+            connex, addrVotingContract,
+            utils.getABI(abiVotingContract, 'getVoter', 'function'),
+            voteID, i
+        )
+        const addr = out.decoded[0]
+
+        // download compressed ballot
+        out = await connexutils.contractCall(
+            connex, addrVotingContract,
+            utils.getABI(abiVotingContract, 'getBallot', 'function'),
+            voteID, addr
+        )
+        const cb = getCompressedBallot(out)
+        
+        const b = uncompressBallot(cb, addr, gk)
+
+        tally.count(b)
+    }
+
+    const r = prepTallyRes(tally.getRes())
+
+    // Submit tally result
     resp = await connexutils.contractCallWithTx(
         connex, auth, 1000000,
         addrVotingContract, '0x0',
         utils.getABI(abiVotingContract, 'setTallyRes', 'function'),
         voteID,
-        invalidBallots,
-        r.v, r.x, r.y, r.proof, r.prefix
+        r.invalidBallots,
+        r.V, r.X, r.Y, r.zkp, r.prefix
     )
     rec = await connexutils.getReceipt(connex, 5, resp.txid)
-    console.log('Tally result uploaded:')
-    console.log('\ttxid: ', resp.txid)
-    console.log('\tgas used:', rec.gasUsed)
+    console.log(`Tally result submitted:
+    txid: ${resp.txid}
+    gas used: ${rec.gasUsed}`)
 
     // Tally ends
     resp = await connexutils.contractCallWithTx(
@@ -61,27 +129,7 @@ import {
         voteID
     )
     rec = await connexutils.getReceipt(connex, 5, resp.txid)
-    console.log('Tally ends:')
-    console.log('\ttxid: ', resp.txid)
-    console.log('\tgas used:', rec.gasUsed)
-
-    driver.close()
+    console.log(`Tally ends:
+    txid: ${resp.txid}
+    gas used: ${rec.gasUsed}`)
 })()
-
-function prepTallyRes(r: any): any {
-    const p = r.proof
-
-    let prefix: string = '0x'
-    prefix += pre(r.xy)
-    prefix += pre(r.yy)
-    prefix += pre(p.hy)
-    prefix += pre(p.ty)
-
-    return {
-        v: r.v,
-        x: r.xx,
-        y: r.yx,
-        proof: [p.hx, p.tx, p.r],
-        prefix: prefix
-    }
-}
